dd7210d920657c0da5d1f876a816db3dedf24713
Damian Holona <damianholona074@gmail.com>
2026-01-02 04:16:41 +0100

    [MED-02] correct PatientMapper

--- src/main/java/com/dname074/medicalclinic/mapper/PatientMapper.java
+++ src/main/java/com/dname074/medicalclinic/mapper/PatientMapper.java
@@ -5,10 +5,17 @@ import com.dname074.medicalclinic.model.CreatePatientCommand;
 import com.dname074.medicalclinic.model.Patient;
 import com.dname074.medicalclinic.model.PatientDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface PatientMapper {
     Patient createPatientCommandToEntity(CreatePatientCommand patientCommand);
-    String changePasswordCommandToEntity(ChangePasswordCommand passwordCommand);
+    default String changePasswordCommandToEntity(ChangePasswordCommand passwordCommand) {
+        if (passwordCommand == null) {
+            return null;
+        }
+        return passwordCommand.password();
+    }
+    @Mapping(target = "surname", source = "patient.lastName")
     PatientDto toDto(Patient patient);
 }
--- src/main/java/com/dname074/medicalclinic/model/PatientDto.java
+++ src/main/java/com/dname074/medicalclinic/model/PatientDto.java
@@ -11,16 +11,7 @@ public class PatientDto {
     private final String email;
     private final int idCardNo;
     private final String firstName;
-    private final String lastName;
+    private final String surname;
     private final String phoneNumber;
     private final LocalDate birthday;
-
-    public PatientDto(Patient patient) {
-        this.email = patient.getEmail();
-        this.idCardNo = patient.getIdCardNo();
-        this.firstName = patient.getFirstName();
-        this.lastName = patient.getLastName();
-        this.phoneNumber = patient.getPhoneNumber();
-        this.birthday = patient.getBirthday();
-    }
 }
