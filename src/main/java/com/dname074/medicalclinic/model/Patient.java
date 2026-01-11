6a717e2abdcbcd5f0e7ff77ed45c09fc961cf8fd
Damian Holona <damianholona074@gmail.com>
2026-01-11 23:19:10 +0100

    [MED-03] add entity annotations to patient class

--- src/main/java/com/dname074/medicalclinic/model/Patient.java
+++ src/main/java/com/dname074/medicalclinic/model/Patient.java
@@ -1,5 +1,11 @@
 package com.dname074.medicalclinic.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,13 +15,23 @@ import java.time.LocalDate;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name="patient")
 public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idCardNo;
+    @Column(name="email")
     private String email;
+    @Column(name="password")
     private String password;
-    private int idCardNo;
+    @Column(name="firstName")
     private String firstName;
+    @Column(name="lastName")
     private String lastName;
+    @Column(name="phoneNumber")
     private String phoneNumber;
+    @Column(name="birthday")
     private LocalDate birthday;
 
     public void update(Patient patient) {
