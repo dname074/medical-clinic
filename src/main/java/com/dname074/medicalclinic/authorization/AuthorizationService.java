bbaa065de10c53578882638899c7929fb0335870
Damian Holona <damianholona074@gmail.com>
2026-04-25 03:00:28 +0200

    [MED-11] change environment section in docker compose

--- src/main/java/com/dname074/medicalclinic/authorization/AuthorizationService.java
+++ src/main/java/com/dname074/medicalclinic/authorization/AuthorizationService.java
@@ -1,7 +1,6 @@
 package com.dname074.medicalclinic.authorization;
 
 import com.dname074.medicalclinic.model.Role;
-import com.dname074.medicalclinic.model.VisitStatus;
 import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -9,20 +8,6 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class AuthorizationService {
-    public boolean canAccessDoctorVisits(VisitStatus status, Authentication auth) {
-        boolean isDoctorOrAdmin = auth.getAuthorities().stream()
-                .anyMatch(a -> a.getAuthority().equals("ROLE_DOCTOR")
-                                                        || a.getAuthority().equals("ROLE_ADMIN"));
-        boolean isPatient = auth.getAuthorities().stream()
-                .anyMatch(a -> a.getAuthority().equals("ROLE_PATIENT"));
-
-        if (status == VisitStatus.AVAILABLE) {
-            return isPatient || isDoctorOrAdmin;
-        }
-
-        return isDoctorOrAdmin;
-    }
-
     public Jwt extractJwt(Authentication auth) {
         if (auth == null || !(auth.getPrincipal() instanceof Jwt jwt)) {
             throw new AuthenticationCredentialsNotFoundException("Invalid token");
